b804bdd0382e9cb430713f18847d023897647a2f
mpassava <124836581+mpassava@users.noreply.github.com>
2023-04-01 13:06:46 -0400

    Added Lead Tracker v1.0 files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+let myLeads = [];
+
+const inputBtn = document.getElementById('input-btn');
+const inputEl = document.getElementById('input-el');
+const ulEl = document.getElementById('ul-el');
+const deleteBTN = document.getElementById('delete-btn');
+const tabBTN = document.getElementById('tab-btn');
+const LeadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
+
+if (LeadsFromLocalStorage) {
+    myLeads = JSON.parse(localStorage.getItem('myLeads'));
+    render(myLeads);
+}
+
+function render(leads) {
+    let listItems = '';
+    for (i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target="_blank" rel="noopener noreferrer" href="${leads[i]}">
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems;
+}
+
+deleteBTN.addEventListener('dblclick', function() {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+})
+
+inputBtn.addEventListener('click', function() {
+    myLeads.push(inputEl.value);
+    inputEl.value = '';
+    localStorage.setItem('myLeads', JSON.stringify(myLeads));
+    render(myLeads);
+})
+
+tabBTN.addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url);
+        localStorage.setItem('myLeads', JSON.stringify(myLeads));
+        render(myLeads);
+    })
+})
